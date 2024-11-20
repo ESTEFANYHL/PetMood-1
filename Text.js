2e92e4520afd99f9ca00d6d3c63edf78ad9e015f
LuisAlbertoOlivaC <luisaolivac23@gmail.com>
2024-11-20 11:18:39 -0600

    Textos definidos para resultados
    Recomendaciones para el resultado que nos de el dataset

---
+++ Text.js
@@ -0,0 +1,30 @@
+import React from "react"
+import {Text} from "react-native"
+
+export default function Triste(){
+    <view>
+        <Text>Dedica más tiempo y atención. Pasar tiempo de calidad con él puede ayudarlo a sentirse más seguro y querido. Esto incluye actividades como caricias, palabras amables y juegos interactivos. El ejercicio diario es clave, ya que salir a pasear, correr o jugar en el parque no solo mejora su condición física, sino que también estimula su mente y le permite distraerse. Introduce juguetes interactivos o juegos de olfato para mantenerlo ocupado y mentalmente activo, ya que la falta de estimulación puede contribuir a su tristeza. La socialización también es fundamental; 
+            intenta organizar encuentros con otros perros o permite que conozca personas nuevas en un ambiente seguro y positivo. </Text>
+    </view>
+}
+
+export default function Enojado(){
+    <view>
+        <Text>Un perro enojado necesita primero un entorno tranquilo. Es esencial que tú, como su guía, mantengas la calma, ya que los perros perciben y reflejan nuestras emociones. Identifica qué está provocando su enojo; puede ser miedo, frustración, territorios invadidos u otros estímulos externos. Retira o minimiza el desencadenante, si es posible. Ayúdalo a relajarse utilizando técnicas de entrenamiento positivo: 
+            premia las conductas calmadas con golosinas, caricias o palabras suaves para reforzar un comportamiento tranquilo.  </Text>
+    </view>
+}
+
+export default function Feliz(){
+    <view>
+        <Text>Para seguir manteniendo a tu perro feliz es importante brindarle amor y atención diaria, dedicando tiempo de calidad para jugar, acariciarlo y reforzar el vínculo emocional. Asegúrate de que realice ejercicio regular mediante paseos, carreras o juegos, y proporciona estimulación mental con juguetes interactivos, retos de olfato y entrenamiento de trucos. 
+            Una dieta equilibrada, adaptada a su raza, tamaño y edad, también es fundamental para su bienestar</Text>
+    </view>
+}
+
+export default function Relajado(){
+    <view>
+        <Text>Para mantener a un perro relajado y sin estrés, es esencial proporcionarle un entorno tranquilo con un espacio seguro donde pueda refugiarse si se siente abrumado, como una cama en un rincón apartado. Mantén una rutina diaria predecible, ya que esto le ayudará a sentirse más seguro, y 
+            libera su energía con paseos y juegos regulares.</Text>
+    </view>
+}
